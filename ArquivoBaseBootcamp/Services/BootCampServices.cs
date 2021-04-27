e0197f7471c730413de9529cf71d6e17b5747484
João Gabriel Kasprik Arruda <joaoga@pbtech.net.br>
2021-04-27 15:00:59 -0300

    Add project files.

---
+++ ArquivoBaseBootcamp/Services/BootCampServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArquivoBaseBootcamp.Services
+{
+    public class BootCampServices : IBootCampServices
+    {
+        public string ConsultarInteressada()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConsultarTodasInteressadas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CadastrarInteressada()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AtualizarInteressada()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExcluirInteressada()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
